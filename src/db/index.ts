c40e6fff14319fadc15aab8c081e113f1c74a4f7
Tindecken <tindecken@gmail.com>
2025-11-09 12:26:35 +0700

    a

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,10 +3,13 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "../../drizzle/schema";
 
 console.log('process.env.TURSO_DATABASE_URL:', process.env.TURSO_DATABASE_URL);
+console.log('process.env.TURSO_AUTH_TOKEN:', process.env.TURSO_AUTH_TOKEN);
+console.log('process.env.PORT:', process.env.PORT);
+
 
 const client = createClient({
-  url: Bun.env.TURSO_DATABASE_URL!,
-  authToken: Bun.env.TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 export const db = drizzle(client, { schema });
