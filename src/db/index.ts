046072b2423d1688c418305820482514aec62466
Tindecken <tindecken@gmail.com>
2025-11-09 11:57:39 +0700

    q

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,8 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "../../drizzle/schema";
 
+console.log('Bun.env.TURSO_DATABASE_URL:', Bun.env.TURSO_DATABASE_URL);
+
 const client = createClient({
   url: Bun.env.TURSO_DATABASE_URL!,
   authToken: Bun.env.TURSO_AUTH_TOKEN,
