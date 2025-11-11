62b5bee6f86d47913251699d91144b337952f734
hnu <tindecken@gmail.com>
2025-11-11 10:22:15 +0700

    A

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,26 +1,18 @@
+
 module.exports = {
-  apps: [
-    {
-      app: "tind_tracking_api",
-      name: 'tind_tracking_api',
-      script: 'src/index.ts',
-      interpreter: '/root/.bun/bin/bun',
-      interpreter_args: 'run',
-      instances: 1,
-      exec_mode: 'fork',
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 9200,
-      },
-      error_file: 'pm2-error.log',
-      out_file: 'pm2-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '500M',
-      min_uptime: '10s',
-      max_restarts: 10,
-    },
-  ],
+  name: "tind_tracking",
+  title: "tind_tracking", // Name of your application
+  script: "src/index.ts", // Entry point of your application
+  interpreter: "/root/.bun/bin/bun", // Bun interpreter
+  env_production: {
+	NODE_ENV: "production"
+  },
+  
+  env: {
+    PATH: `${process.env.HOME}//root/.bun/bin/bun:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
+  },
+  error_file: 'pm2-error.log',
+  out_file: 'pm2-out.log',
+  log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+  merge_logs: true,
 };
