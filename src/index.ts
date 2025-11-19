6460eda080c2068a176836de4edbd9f118e3e593
Tindecken <tindecken@gmail.com>
2025-11-19 20:06:29 +0700

    add api

--- src/index.ts
+++ src/index.ts
@@ -19,19 +19,19 @@ const app = new Hono();
 
 app.get("/", (c) => c.text("✅ Tind Tracking API works"));
 app.get("/api1", async (c) => {
-  await new Promise((resolve) => setTimeout(resolve, 100));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return c.text("API 1 worked");
 });
 app.get("/api2", async (c) => {
-  await new Promise((resolve) => setTimeout(resolve, 200));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return c.text("API 2 worked");
 });
 app.get("/api3", async (c) => {
-  await new Promise((resolve) => setTimeout(resolve, 300));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return c.text("API 3 worked");
 });
 app.get("/api4", async (c) => {
-  await new Promise((resolve) => setTimeout(resolve, 400));
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   return c.text("API 4 worked");
 });
 app.route("/transactions", addTransactionRoute);
